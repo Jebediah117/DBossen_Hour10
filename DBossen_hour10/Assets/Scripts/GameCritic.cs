09536090705565f1cd47d34477b254cdad641f0b
Jebediah117 <dylanbossen@gmail.com>
2025-11-09 19:28:44 -0800

    Created ChaosBall scene from hour 10

---
+++ DBossen_hour10/Assets/Scripts/GameCritic.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCritic : MonoBehaviour
+{
+    /*
+     The 4 suggestions I did are:
+    1.Try adding variations of the chaos balls 
+    added 2 more balls with different sizes and speed
+    2.Try changing the size and shape of the player's bumper
+    added 2 cylinder bumpers as well as more rectangles of different sizes
+    3.try making a complex bumper out of many shapes
+    added a doorway style obstacle and a archway style obstacle
+    4.try adding something over the water outside the arena
+    created stands outside the arena
+    */
+}
