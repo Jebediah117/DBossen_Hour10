53f959b93fda2ec4acad00df4d2f60fac028f525
Jebediah117 <dylanbossen@gmail.com>
2025-11-12 15:45:27 -0800

    Added answers to game critic script

--- DBossen_hour10/Assets/Scripts/GameCritic.cs
+++ DBossen_hour10/Assets/Scripts/GameCritic.cs
@@ -5,6 +5,25 @@ using UnityEngine;
 public class GameCritic : MonoBehaviour
 {
     /*
+    I do think the game is too easy and it's because of how speratic the balls
+    are. Some of the balls go in instantly before you even have any chance of
+    noticing while others take forever to go in. 
+
+    I think something that would make it harder is more obstacles. With more 
+    obstacles there are more things for the balls to get stuck on and so more time
+    before they end up in the goals.
+
+    I think something that would give it a wow factor is fun sound effects. The game
+    is simialir to pinball, so having some similair sound effects would make it pop
+    more. Having different ones for hitting walls, balls, or obstacles would add a 
+    lot.
+
+    The part of the game that is fun is seeing the balls bounce around and watching
+    them deal with obstacles. A ball may be about to go in and then get knocked out
+    of the way. The part that is tedious is when the balls get stuck in a loop and
+    take a while to get out. Early into making it that would happen a lot with the
+    ball never coming close to going in because its in a loop.
+
      The 4 suggestions I did are:
     1.Try adding variations of the chaos balls 
     added 2 more balls with different sizes and speed
